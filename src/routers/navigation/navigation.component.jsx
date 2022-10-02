1a90ec271794baf070d56393e8fbeaf40c56f8e9
BigiCrab <jerry850408@gmail.com>
2022-10-02 16:55:03 +0800

    87 import svg as component add style

--- src/routers/navigation/navigation.component.jsx
+++ src/routers/navigation/navigation.component.jsx
@@ -1,13 +1,21 @@
 import { Outlet, Link } from "react-router-dom";
+import "./navigation.styles.scss";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 
 const Navigation = () => {
   return (
     <>
-      <nav>
-        <Link to={"/"}>LOGO</Link>
+      <nav className="navigation">
+        <Link to={"/"}>
+          <div className="logo-container">
+            <CrwnLogo className="logo"></CrwnLogo>
+          </div>
+        </Link>
         <ul>
-          <li>
-            <Link to={"/shop"}>SHOP</Link>
+          <li className="nav-links-container">
+            <Link className="nav-link" to={"/shop"}>
+              SHOP
+            </Link>
           </li>
         </ul>
       </nav>
